8137af8b07fdb5cd77e80d3e0d14a1cc2e4fece8
Ismail Bello <ismail.bello.513@gmail.com>
2021-06-03 23:30:19 +0100

    move pre-commit hook to nix-shell

--- shell.nix
+++ shell.nix
@@ -3,8 +3,28 @@ let
 
   haskellPackages = hs: with hs; [ hoogle hlint stylish-haskell hspec ];
 
+  pre-commit = pkgs.writeScriptBin "pre-commit" ''
+    !${pkgs.stdenv.shell}
+    # hs-lint
+    echo "auto-formatting all hs files"
+    stylish-haskell -v -i $(fd hs --type file | xargs) && hlint . || exit 1
+
+    echo "formatting all nix files"
+    nixfmt --width=80 $(fd '.nix' --type file | xargs) || exit 1
+  '';
+
 in pkgs.mkShell {
 
-  buildInputs = [ pkgs.nixfmt pkgs.fd (pkgs.ghc.withPackages haskellPackages) ];
+
+  buildInputs = [
+    pkgs.nixfmt
+    pkgs.fd
+    pre-commit
+    (pkgs.ghc.withPackages haskellPackages)
+  ];
+
+  shellHook = ''
+    ln -fs ${pre-commit}/bin/pre-commit ./.git/hooks/pre-commit
+  '';
 
 }
